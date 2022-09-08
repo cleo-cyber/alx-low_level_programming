a6fe8a43ead55bbd610dc21fe43034b78e362077
cleo-cyber <kadimacleo92@gmail.com>
2022-09-08 16:54:47 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main(void) /*This is the entry point*/
 {
 
-puts("\"Programming is like building a multilingual puzzle"); /* acts as a print function*/
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0); /*end of execution */
 
