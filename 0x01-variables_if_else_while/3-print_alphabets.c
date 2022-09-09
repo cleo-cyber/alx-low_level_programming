46623c3089c34b9360a147a3d6790e57897333fb
cleo-cyber <kadimacleo92@gmail.com>
2022-09-09 16:42:06 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+
+		putchar(i);
+
+	for (i = 'A'; i  <= 'Z'; ++i)
+
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+
+}
