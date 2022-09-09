c9eb3bd133931136c018e078c5fe40dbb749ec91
cleo-cyber <kadimacleo92@gmail.com>
2022-09-09 16:50:40 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description:  print the letters of the alphabet except e, q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+
+	{
+
+		if (i == 'e' && i == 'q')
+
+		{
+			continue;
+
+
+		}
+		else
+		{
+			putchar(i);
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
