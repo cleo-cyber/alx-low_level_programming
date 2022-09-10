569c0c68140e62ce95c5ac3debe623f4efd378f1
cleo-cyber <kadimacleo92@gmail.com>
2022-09-09 17:01:13 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: print the single digits of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+
+		printf("%i", i);
+
+	}
+
+	{
+
+		printf("\n");
+
+	}
+
+	return (0);
+
+}
