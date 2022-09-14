02242e8dabc36c67263b63db5665f20fd1730f6d
cleo-cyber <kadimacleo92@gmail.com>
2022-09-14 13:42:44 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * Return: a-z x10
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (c = 'a'; c <= z; c++)
+		{
+		       _putchar(c);
+		}
+		_putchar(c);
+	}
