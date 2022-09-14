5d7a085e03982870dfddc3ed9cc77e9d4db7bf7b
cleo-cyber <kadimacleo92@gmail.com>
2022-09-14 13:46:15 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,32 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabets 10 times
+ *print_alphabet_x10 - function to print abc 10 times
  *
- * Return: a-z x10
+ * Return: 0
+ *
  */
+
 void print_alphabet_x10(void)
+
 {
+
 	char c, i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
+
 	{
-		for (c = 'a'; c <= z; c++)
+
+		for (c = 'a'; c <= 'z'; c++)
+
 		{
-		       _putchar(c);
+
+			_putchar(c);
+
 		}
-		_putchar(c);
+
+		_putchar('\n');
+
 	}
+
+}
