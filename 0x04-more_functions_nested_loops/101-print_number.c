aeb0820166ece262def23714dc5675612417056a
cleo-cyber <kadimacleo92@gmail.com>
2022-09-15 15:17:44 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - function to print an integer
+ *
+ * @n: parameter
+ *
+ * Return: 0
+ *
+ */
+void print_number(int n)
+
+{
+
+	n = 9;
+
+	_putchar(i);
+
+	return (0);
+
+}
